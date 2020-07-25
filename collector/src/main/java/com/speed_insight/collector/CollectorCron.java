a6c76ab9e88e46ed977f7a695715a958bdd10f54
gwkim <youlive789@gmail.com>
2020-07-25 23:54:48 +0900

    feat: Fix process builder string bug

--- collector/src/main/java/com/speed_insight/collector/CollectorCron.java
+++ collector/src/main/java/com/speed_insight/collector/CollectorCron.java
@@ -1,13 +1,17 @@
 package com.speed_insight.collector;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import com.speed_insight.collector.utils.CollectTargetParser;
 
@@ -18,7 +22,7 @@ public class CollectorCron {
 	
 	private String shellCmd;
 	//private final static String LIGHTHOUSE_CMD = "docker run --rm lighthouse ";
-	private final static String LIGHTHOUSE_CMD = "/bin/sh -c '/usr/bin/lighthouse --output json --throttling-method=provided --chrome-flags=\"--headless --no-sandbox\" --quiet --only-categories=performance --emulated-form-factor=";
+	private final static String LIGHTHOUSE_CMD = "/usr/bin/lighthouse --output json --throttling-method=provided --chrome-flags=\"--headless --no-sandbox\" --quiet --only-categories=performance --emulated-form-factor=";
 	
 	public CollectorCron() {
 		super();
@@ -48,8 +52,16 @@ public class CollectorCron {
 		}
 	}
 	
-	private void executeCommand(String deviceFlag) {
-		Runtime rt = Runtime.getRuntime();
+	private void executeCommand(String deviceFlag) {		
+		Process process = null;
+		ProcessBuilder pb = null;
+		StringBuffer successOutput = new StringBuffer();
+        StringBuffer errorOutput = new StringBuffer();
+		BufferedReader successBufferReader = null;
+		BufferedReader errorBufferReader = null;
+		String msg = null;
+		
+		List<String> command = null;
 		
 		File tmpPath = new File("../data/tmp");
 		if (!tmpPath.exists()) tmpPath.mkdir();
@@ -57,14 +69,61 @@ public class CollectorCron {
 		for (String target : this.targetList) {
 			System.out.println("Now Target : " + target);
 			try {
-				String currentCmd = LIGHTHOUSE_CMD + deviceFlag + " " + target + " > ../data/tmp/" + this.getDateString() + "." + deviceFlag + ".json'";
-				System.out.println(currentCmd);
-				Process process = rt.exec(currentCmd);
+				
+				String currentCmd = LIGHTHOUSE_CMD + deviceFlag + " " + target + " > ../data/tmp/" + this.getDateString() + "." + deviceFlag + ".json";
+				
+				command = new ArrayList<String>();
+				command.add("/bin/sh");
+				command.add("-c");
+				command.add(currentCmd);
+				
+				pb = new ProcessBuilder(command);
+				process = pb.start();
+
+				// shell 실행이 정상 동작했을 경우
+	            successBufferReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+	            while ((msg = successBufferReader.readLine()) != null) {
+	                successOutput.append(msg + System.getProperty("line.separator"));
+	            }
+	 
+	            // shell 실행시 에러가 발생했을 경우
+	            errorBufferReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+	            while ((msg = errorBufferReader.readLine()) != null) {
+	                errorOutput.append(msg + System.getProperty("line.separator"));
+	            }
+				
 				process.waitFor();
+				
+				// shell 실행이 정상 종료되었을 경우
+	            if (process.exitValue() == 0) {
+	                System.out.println("성공 " + process.exitValue());
+	                System.out.println(successOutput.toString());
+	            } else {
+	                // shell 실행이 비정상 종료되었을 경우
+	                System.out.println("비정상 종료 " + process.exitValue());
+	                System.out.println(successOutput.toString());
+	            }
+	 
+	            // shell 실행시 에러가 발생
+	            if (!StringUtils.isEmpty(errorOutput.toString())) {
+	                // shell 실행이 비정상 종료되었을 경우
+	                System.out.println("오류");
+	                System.out.println(errorOutput.toString());
+	            }
+
 			} 
 			catch (IOException | InterruptedException e) {
 				e.printStackTrace();
 			} 
+			finally {
+				try {
+	                process.destroy();
+	                if (successBufferReader != null) successBufferReader.close();
+	                if (errorBufferReader != null) errorBufferReader.close();
+	            } catch (IOException e1) {
+	                e1.printStackTrace();
+	            }
+			}
 		}
 	}
 	
